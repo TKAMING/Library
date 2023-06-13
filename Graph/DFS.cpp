07e56e6d74cd92e504e62bcec9d45a360a73d098
Tobias Karuth <98485613+TKAMING@users.noreply.github.com>
2023-06-13 20:26:13 +0200

    Update DFS.cpp

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -1,1 +1,13 @@
-
+void DFS (int vertex) {
+    visited[vertex] = trueL
+    list<int> adjList = adjList[vertex];
+  
+    cout << vertex;
+    
+    list<int>::iterator i;
+    for (i = adjList.begin(); i != adjList.end; i++) {
+        if (!visited[*i]) {
+            DFS(*i);
+        }
+    }
+}
