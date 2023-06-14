fd8187aa5e81af033718385d95615b6954abbdc2
TKAMING <tobiaskaruth@gmail.com>
2023-06-14 06:13:05 +0200

    add BFS.cpp

--- Graph/BFS/BFS.cpp
+++ Graph/BFS/BFS.cpp
@@ -1,1 +1,26 @@
+void Graph::BFS(int vertex) {
+  visited = new bool[numVertices];
+  for (int i = 0; i < numVertices; i++)
+    visited[i] = false;
 
+  list<int> queue;
+
+  visited[vertex] = true;
+  queue.push_back(vertex);
+
+  list<int>::iterator i;
+
+  while (!queue.empty()) {
+    int currVertex = queue.front();
+    cout << "Visited " << currVertex << " ";
+    queue.pop_front();
+
+    for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
+      int adjVertex = *i;
+      if (!visited[adjVertex]) {
+        visited[adjVertex] = true;
+        queue.push_back(adjVertex);
+      }
+    }
+  }
+}
