47af9c2d7553e8b15601907de71309b741120fe9
TKAMING <tobiaskaruth@gmail.com>
2023-06-14 06:28:43 +0200

    add Binary search

---
+++ Searching&Sorting/Binarysearch/Binarysearch.cpp
@@ -0,0 +1,18 @@
+int binarySearch (int array[], int size, int SearchValue) {
+    int low = 0;
+    int high = size - 1;
+    int mid;
+    while (low <= high) {
+        mid = (low + high)/2';
+        if (SearchValue == array[mid]) {
+            return mid;
+        }
+        else if (SearchValue > array[mid]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
